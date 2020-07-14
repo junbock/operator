91316bf310dc702a05d50f6385d1965e8589d9f4
엄재식 <skokk33@gmail.com>
2020-07-14 17:22:57 +0900

    test case 추가

--- src/operator/Test.java
+++ src/operator/Test.java
@@ -6,11 +6,15 @@ public class Test {
 		Calculator div = new Div();
 		//1.5 출력
 		System.out.println(div.calculate(3, 2));
+		//0 div 에러 
+		//System.out.println(div.calculate(4, 0));
 		
 		//combination test
 		Calculator com = new Combination();
-		//정상 동작 12 출력
+		//정상 동작 1) 12 출력
 		System.out.println(com.calculate(4, 2));
+		//정상 동작 2) 1 출력
+		System.out.println(com.calculate(5, 5));
 		//잘못된 입력 : a<b, -1 출력
 		System.out.println(com.calculate(2, 4));
 		//잘못된 입력 : a or b 자연수 아님, -1 출력
