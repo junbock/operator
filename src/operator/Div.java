97d49255dd9d7f9e48deaab5a921ed80dba08bba
엄재식 <skokk33@gmail.com>
2020-07-14 15:40:45 +0900

    div test 추가

--- src/operator/Div.java
+++ src/operator/Div.java
@@ -2,6 +2,6 @@ package operator;
 
 public class Div implements Calculator {
 	public double calculate(int num1, int num2) {
-		return num1/num2;
+		return (double)num1/num2;
 	}
 }
---
+++ src/operator/Test.java
@@ -0,0 +1,9 @@
+package operator;
+
+public class Test {
+	public static void main(String[] args) {
+		Calculator div = new Div();
+		System.out.println(div.calculate(3, 2));
+		
+	}
+}
