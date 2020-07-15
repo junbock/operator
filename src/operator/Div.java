2323c919e4ff4f6d48e6b72c84cc556ac8ddddb5
엄재식 <skokk33@gmail.com>
2020-07-15 17:52:42 +0900

    나눗셈 0으로 나누기처리

--- src/operator/Div.java
+++ src/operator/Div.java
@@ -1,7 +1,12 @@
 package operator;
-
+/* 나눗셈
+ * 분모가 0이면 0을 반환
+ */
 public class Div implements Calculator {
 	public double calculate(int num1, int num2) {
+		if (num2 == 0) {
+			return 0.0;
+		}
 		return (double)num1/num2;
 	}
 }
