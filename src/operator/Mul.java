c2a4df96861b4cc3360d31147370287570bd4ec4
chaeyeon111 <chloepark9811@gmail.com>
2020-07-15 11:56:37 +0900

    Revert "init_commit"
    This reverts commit eecbde29c984cfb4b47e1b871ac2ea132b257166.

--- src/operator/Mul.java
+++ src/operator/Mul.java
@@ -1,10 +1,7 @@
 package operator;
 
-public class mul {
-
-	public static Object calculate(int num1, int num2) {
-		
-		return (double)num1 * num2;
+public class Mul implements Calculator{
+	public double calculate(int num1, int num2) {
+		return (double)num1*num2;
 	}
-
 }
